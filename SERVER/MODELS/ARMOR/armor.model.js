64415b911819e8c104f59457a2d81c1346be1b06
@rcplaza <rcplaza.z@gmail.com>
2018-10-01 16:35:56 -0300

    Monday Afternoon - Schemas Update
    UPDATE accesorySchema.
    UPDATE armorSchema.
    UPDATE armsSchema.
    UPDATE helmetSchema.
    UPDATE legsSchema

--- SERVER/MODELS/ARMOR/armor.model.js
+++ SERVER/MODELS/ARMOR/armor.model.js
@@ -7,8 +7,20 @@ let armorSchema = new Schema({
     physicsDefese: { type: Number, required: false },
     magicDefense: { type: Number, required: false },
     doge: { type: Number, required: false },
-    stat: { type: String, required: false },
-    quantityStat: { type: Number, required: false }
+    bonus: {
+        stat: { type: String, require: false },
+        quantityStat: { type: Number, required: false },
+        secondStat: { type: String, require: false },
+        quantitySecondStat: { type: Number, required: false },
+        thirdStat: { type: String, require: false },
+        quantityThirdStat: { type: Number, required: false }
+    },
+    requirements: {
+        typeClass: { type: String, required: true },
+        SecondTypeClass: { type: String, required: false },
+        level: { type: Number, required: true }
+    },
+    lore: { type: String, required: true }
 });
 
 module.exports = mongoose.Schema('armor', armorSchema);
